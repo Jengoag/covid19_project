c2e55777847c9ce952ae8193e11138ea2882c149
Jennifer Gomez <jennifergoag@gmail.com>
2022-01-01 19:50:00 +0100

    creadas tablas directas a sql en sql.py

--- api/database/sql.py
+++ api/database/sql.py
@@ -11,3 +11,35 @@ postgres_url = f'postgresql+psycopg2://{user}:{password}@127.0.0.1:5432'
 
 engine = create_engine(f'{postgres_url}/covid_19')
 db = engine.connect()
+
+import pandas as pd
+
+# Tabla confirmed
+covid_19 = pd.read_csv('../../data/clean/confirmed.csv')
+covid_19.columns = [c.lower() for c in covid_19.columns] 
+
+covid_19.to_sql("confirmed", engine)
+
+# Tabla deaths
+covid_19 = pd.read_csv('../../data/clean/deaths.csv')
+covid_19.columns = [c.lower() for c in covid_19.columns] 
+
+covid_19.to_sql("deaths", engine)
+
+# Tabla geolocation
+covid_19 = pd.read_csv('../../data/clean/geolocation.csv')
+covid_19.columns = [c.lower() for c in covid_19.columns] 
+
+covid_19.to_sql("geolocation", engine)
+
+# Tabla recovered
+covid_19 = pd.read_csv('../../data/clean/recovered.csv')
+covid_19.columns = [c.lower() for c in covid_19.columns] 
+
+covid_19.to_sql("recovered", engine)
+
+# Tabla spending
+covid_19 = pd.read_csv('../../data/clean/spending.csv')
+covid_19.columns = [c.lower() for c in covid_19.columns] 
+
+covid_19.to_sql("spending", engine)
