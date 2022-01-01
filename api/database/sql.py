069180661c17efb26f117f9c977f34cdc5ba79c6
Jennifer Gomez <jennifergoag@gmail.com>
2022-01-01 19:09:29 +0100

    archivo sql.py

--- api/database/sql.py
+++ api/database/sql.py
@@ -7,7 +7,7 @@ password = os.getenv("POSTGRES_PASSWORD")
 
 from sqlalchemy import create_engine
 
-postgres_url = f'postgresql+psycopg2://{user}:{password}@127.0.0.1:5433'
+postgres_url = f'postgresql+psycopg2://{user}:{password}@127.0.0.1:5432'
 
 engine = create_engine(f'{postgres_url}/covid_19')
 db = engine.connect()
